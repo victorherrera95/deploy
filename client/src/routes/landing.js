613f72f55f39a5ae84535d9533b6e9f337a06137
victorherrera95 <herreracontrerasva@gmail.com>
2023-01-19 14:35:31 -0300

    bg

--- client/src/routes/landing.js
+++ client/src/routes/landing.js
@@ -14,7 +14,7 @@ const LandingPage = () =>{
 }
 
 const BodyPage = styled.div`
-  background-image: url("C:\Users\herre\Desktop\deploy\client\src\img\20093-perritos.png");
+  
   background-position: 20% 65%; 
   background-attachment: fixed;
   height: 650px;

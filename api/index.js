5237c9f8fbf1555b9fa59e09d1598cc5ed956134
victorherrera95 <herreracontrerasva@gmail.com>
2023-01-19 20:30:55 -0300

    port

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ require("dotenv").config()
 
 // Syncing all the models at once.
 conn.sync({ force: false}).then(() => {
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });

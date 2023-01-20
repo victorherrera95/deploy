ea8449516c63091d98ecf9184041065c423a0499
victorherrera95 <herreracontrerasva@gmail.com>
2023-01-19 21:26:00 -0300

    db

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ require("dotenv").config()
 // Syncing all the models at once.
 conn.sync({ force: false}).then(() => {
   server.listen(process.env.PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log('%s listening at', process.env.PORT); // eslint-disable-line no-console
   });
 });
